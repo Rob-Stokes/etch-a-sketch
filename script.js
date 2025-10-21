8bac2d4cc5c7a92d2c6af128ab8b69ac4bdd3206
Rob Stokes <176937338+Rob-Stokes@users.noreply.github.com>
2025-10-21 16:40:44 +0100

    finish styling 16x16 grid

--- script.js
+++ script.js
@@ -3,6 +3,5 @@ const gridContainer = document.querySelector(".grid-container");
 
 for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
-    div.setAttribute("style", "border: 2px solid black;");
     gridContainer.appendChild(div);
 }
