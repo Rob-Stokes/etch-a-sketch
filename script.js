d30f316edff0380d8786cae3cbd718c5dbddaf4e
Rob Stokes <176937338+Rob-Stokes@users.noreply.github.com>
2025-10-22 13:19:27 +0100

    ?

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     gridContainer.appendChild(div);
 }
+
+    const gridSquares = document.querySelectorAll(".grid-container div");
+    gridSquares.forEach((div) => {
+      div.addEventListener("mouseover", () => {
+      div.classList.add("filled");
+      })
+    });
+
